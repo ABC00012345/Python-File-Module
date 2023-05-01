590511a41fa1e87918c9c30a1e12beafccce9ad4
ABC00012345 <125692659+ABC00012345@users.noreply.github.com>
2023-05-01 19:33:00 +0200

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,17 @@
+@echo off
+cls
+
+read -p "Do you really want to remove the it?" user_input
+
+if [ "$user_input" == "N" ]; then
+    exit
+) else (
+    rm /usr/lib/python3.10/file.py
+    rm /usr/lib/python3.11/file.py
+)
+
+if [ -f /usr/lib/python3.10/file.py -o -f /usr/lib/python3.11/file.py]; then
+    echo "Failed! Plese remove it yourself: /usr/lib/python3.11/ or /usr/lib/python3.10/"
+else
+    echo Successfully removed!
+fi
